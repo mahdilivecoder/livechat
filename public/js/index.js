5bb9dce22e84e3018f342c67983ac890928df0f1
livecoder <m.dev76@yahoo.com>
2018-05-05 21:40:58 +0430

    fix bug on older browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,18 @@
 var socket=io();
-socket.on('connect',()=>{
+socket.on('connect',function (){
     console.log('new user omad!');
 
 });
-socket.on('disconnect',()=>{
+socket.on('disconnect',function (){
     console.log('now user is disconnect!');
 });
-socket.on('newMessage',(newMessage)=>{
+socket.on('newMessage',function (newMessage){
     console.log('newMessage',newMessage);
     var li=$('<li></li>');
     li.text(newMessage.from+':'+newMessage.text);
     $('#message').append(li);
 });
-$('#message-form').on('submit',(e)=>{
+$('#message-form').on('submit',function (e){
     e.preventDefault();
     socket.emit('createMessage',{
         from:'User',
